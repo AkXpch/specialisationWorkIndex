f7841dee0dd0043fd4040e9c2763adff5e959d46
Ice <125239607+AkXpch@users.noreply.github.com>
2026-09-17 02:44:17 +1000

    create button to switch between 2 environment
    bathroom and kitchen

---
+++ main.js
@@ -0,0 +1,12 @@
+const bathroomButton = document.getElementById("bathroom-button");
+const kitchenButton = document.getElementById("kitchen-button");
+
+const bathroomPage = document.getElementById("bathroom-page");
+const kitchenPage = document.getElementById("kitchen-page");
+
+kitchenButton.addEventListener("click", function() {
+    // if "click" kitchen button -> show kitchen page and hide bathroom page
+    kitchenPage.style.display = 'block';
+    bathroomPage.style.display = 'none';
+})
+
